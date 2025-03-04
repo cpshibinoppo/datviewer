55516a5c66208d2250b26b14ffc398652c3b2305
mohammed shibin cp <89115093+cpshibinoppo@users.noreply.github.com>
2025-03-04 10:12:42 +0300

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,14 +5,15 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+    {{-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"> --}}
+    <link href="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.css" rel="stylesheet" />
     <title>Document</title>
 </head>
 
 <body>
     <a href="{{ url('/download') }}" class="btn btn-primary">Download Excel</a>
-    <table class="table">
+    <table class="table" id="search-table">
         <thead>
             <tr>
                 <th scope="col">#</th>
@@ -79,8 +80,8 @@
                         $checkOut = count($times) > 1 ? Carbon::parse($date . ' ' . $times[count($times) - 1]) : null; // Last entry is check-out
 
                         // Define expected check-in and check-out times for that specific date
-                        $expectedCheckIn = Carbon::parse($date . ' 09:30:00'); // Expected check-in time
-                        $expectedCheckOut = Carbon::parse($date . ' 18:30:00'); // Expected check-out time
+                        $expectedCheckIn = Carbon::parse($date . ' 09:00:00'); // Expected check-in time
+                        $expectedCheckOut = Carbon::parse($date . ' 17:30:00'); // Expected check-out time
 
                         // Check if check-in is late or early
                         if ($checkIn->gt($expectedCheckIn)) {
@@ -127,8 +128,20 @@
 
         </tbody>
     </table>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+    {{-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous">
+    </script> --}}
+    <script src="https://cdn.jsdelivr.net/npm/simple-datatables@9.0.3"></script>
+
+    <script src="https://cdn.jsdelivr.net/npm/flowbite@3.1.2/dist/flowbite.min.js"></script>
+
+    <script>
+  if (document.getElementById("search-table") && typeof simpleDatatables.DataTable !== 'undefined') {
+    const dataTable = new simpleDatatables.DataTable("#search-table", {
+        searchable: true,
+        sortable: false
+    });
+}
     </script>
 </body>
 
